0bd9519a7003bb37f613517f93ac676be4c1d5aa
HazimDeveloper <hazimdeveloper@gmail.com>
2024-11-17 19:21:18 +0800

    update new chapter 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,49 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyFirstApp());
+  runApp(MyPortfolio());
 }
 
-class MyFirstApp extends StatelessWidget {
+class MyPortfolio extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('My Awesome App'),
-          backgroundColor: Colors.purple,
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Welcome to Flutter!',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Portfolio'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Welcome to My Portfolio',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
               ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  print('Button pressed!');
-                },
-                child: Text(
-                  'Click Me!',
-                  style: TextStyle(fontSize: 18),
-                ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Flutter Developer',
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.grey[600],
               ),
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            print('Floating button tapped!');
-          },
-          child: Icon(Icons.add),
+            ),
+          ],
         ),
       ),
     );
